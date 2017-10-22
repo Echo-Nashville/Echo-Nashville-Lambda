a913cae1f30f9796a4164e18ddd286df0648f41c
Chase Farmer <chasefarmer2808@ufl.edu>
2017-10-22 01:24:44 -0400

    getting parks with picnics

--- my-service/alexa.js
+++ my-service/alexa.js
@@ -1,6 +1,7 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 const request = require('request');
+const config = require('./config');
 
 module.exports.alexa = function(event, context, callback) {
     const alexa = Alexa.handler(event, context);
@@ -15,7 +16,15 @@ const handlers = {
     'TestIntent': function () {
         var self = this;
 
-        getParksWithPicnics('http://google.com', function(text) {
+        var opt = {
+            url: getParksWithPicnicsUrl(),
+            method: 'GET',
+            headers: {
+                'X-App-Token': config.appToken
+            }
+        };
+
+        getParksWithPicnics(opt, function(text) {
             self.emit(':tell', text);
         })
     },
@@ -24,13 +33,40 @@ const handlers = {
     }
 };
 
-function getParksWithPicnics(url, callback) {
-    request
-        .get(url)
-        .on('response', function (response) {
-            return callback('Getting parks with picnics.');
-        })
-        .on('error', function(err) {
-            console.log(err);
-        });
+function getParksWithPicnics(opt, callback) {
+
+    request(opt, function(err, resp, body) {
+        var speechlet = null;
+
+        if (err) {
+            speechlet = makeParksWithPicnicsSpeechlet([]);
+        } else {
+            speechlet = makeParksWithPicnicsSpeechlet(JSON.parse(body));
+        }
+
+        return callback(speechlet);
+    });
 };
+
+function getParksWithPicnicsUrl() {
+    return 'https://data.nashville.gov/resource/xbru-cfzi.json?$query=SELECT%20park_name,%20picnic_shelters_quantity%20WHERE%20picnic_shelters=%22Yes%22';
+};
+
+function makeParksWithPicnicsSpeechlet(parks) {
+    var parksList = "";
+
+    if (parks.length > 0) {
+        // parks.forEach(function(park) {
+        //     parksList += park.park_name + ' ';
+        // });
+
+        for (var i = 0; i < parks.length; i++) {
+            parksList += parks[i].park_name;
+        }
+
+        var res = `The parks with picnic areas are ${parksList}.`;
+        return res;
+    } else {
+        return 'I could not find any parks with picnic areas.';
+    }
+}
